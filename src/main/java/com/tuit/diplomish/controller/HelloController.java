426a7e183e6f357f63f85fec57d0c53a817d03bf
jack <jahon.080879@gmail.com>
2024-11-01 10:08:08 -0700

    check github actions

--- src/main/java/com/tuit/diplomish/controller/HelloController.java
+++ src/main/java/com/tuit/diplomish/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello World!");
+        return ResponseEntity.ok("Hello World! lets see ");
     }
 }
