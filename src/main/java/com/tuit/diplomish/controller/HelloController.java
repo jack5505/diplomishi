679b10213566b2b5b70559c0c61d5ead578ef9a8
jack <jahon.080879@gmail.com>
2024-11-02 23:11:47 -0700

    lets check it one more time

--- src/main/java/com/tuit/diplomish/controller/HelloController.java
+++ src/main/java/com/tuit/diplomish/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello World! Jack + Abdullox + Sultanboy");
+        return ResponseEntity.ok("Hello Dinora yaxshimisan");
     }
 }
