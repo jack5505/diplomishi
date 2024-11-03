358670260083f60bce9e78df68e90dc8067873f8
jack <jahon.080879@gmail.com>
2024-11-02 23:40:23 -0700

    check

--- src/main/java/com/tuit/diplomish/controller/HelloController.java
+++ src/main/java/com/tuit/diplomish/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello Dinora yaxshimisan????????? Abdullox zo`r bola");
+        return ResponseEntity.ok("Dilshod qachon boshliymiz ?");
     }
 }
