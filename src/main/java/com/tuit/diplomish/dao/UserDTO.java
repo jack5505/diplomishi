734d6776d8a4f4624227dc79e5f50ec023fa261b
jack <jahon.080879@gmail.com>
2024-11-04 10:56:19 -0800

    fix it

--- src/main/java/com/tuit/diplomish/dao/UserDTO.java
+++ src/main/java/com/tuit/diplomish/dao/UserDTO.java
@@ -2,16 +2,52 @@ package com.tuit.diplomish.dao;
 
 import lombok.*;
 
-@Getter
-@Setter
-@Builder
+
 @ToString
-@AllArgsConstructor
-@RequiredArgsConstructor
 public class UserDTO {
-    private Long userId;
+    private Long userId = 0L;
     private String username;
-    private String phone;
+    private String phone = "";
     private String firstName;
     private String lastName;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
