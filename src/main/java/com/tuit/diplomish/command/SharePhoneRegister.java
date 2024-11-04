1c56603e153274f203086d62dd105efbda8b490f
jack <jahon.080879@gmail.com>
2024-11-04 10:34:56 -0800

    add some comment to SharePhoneRegister part

--- src/main/java/com/tuit/diplomish/command/SharePhoneRegister.java
+++ src/main/java/com/tuit/diplomish/command/SharePhoneRegister.java
@@ -5,6 +5,7 @@ import com.tuit.diplomish.dao.UserDTO;
 import com.tuit.diplomish.mapper.ContactToUserDtoMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Contact;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
@@ -27,12 +28,31 @@ public class SharePhoneRegister extends TelegramSendMessage {
         this.telegramClient = telegramClient;
     }
 
+    /**
+     *  In this method we should register new user no matter who is this admin or simple user
+     *  In this first to check this user if this user not exist then write down to database
+     *  otherwise create and let them choose admin or simple user.
+     *  Every time when you enter to Register it will give you choose two options no matter
+     *  your was in our system or not it will keep going to give you two options
+     *
+     * @param update
+     */
+
     @Override
-    public void execute(Update update) {
+    public void execute(Update update)
+    {
+
         log.info(update.toString());
         Contact contact = update.getMessage().getContact();
         UserDTO userDto = mapper.toUserDto(contact);
         userDTOList.add(userDto);
         log.info(userDTOList.toString());
+        //
+        final String welcome_page = String.format("""
+                👨🏼‍💻 WELCOME TO OUR BOT!!!  %s  %s  🥳🤩🤗
+                """, userDto.getLastName(),userDto.getFirstName());
+        SendMessage sendMessage = new SendMessage(update.getMessage().getChatId() + "",welcome_page);
+        responseToMessage(sendMessage);
+
     }
 }
